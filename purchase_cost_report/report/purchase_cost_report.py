97e0c24f117f7197d0207e84ea4d514a2004ab2a
aceleradora-la <info@aceleradora.la>
2026-06-05 16:56:08 -0300

    [FIX] revert a estado estable + fix correcto para purchase_cost_show_button

--- purchase_cost_report/report/purchase_cost_report.py
+++ purchase_cost_report/report/purchase_cost_report.py
@@ -152,18 +152,6 @@ class PurchaseCostReport(models.AbstractModel):
                     }
                 lc_summary_dict[key]["amount_ref"] += lc["amount_ref"]
                 lc_summary_dict[key]["amount_po"] += lc["amount_po"]
-
-        # Calcular tipo de cambio por LC: 1 {po_currency} = X {ref_currency}
-        for lcs in lc_summary_dict.values():
-            if (
-                lcs["ref_currency"] != po_currency
-                and not float_is_zero(lcs["amount_po"],
-                                      precision_rounding=po_currency.rounding)
-            ):
-                lcs["exchange_rate"] = lcs["amount_ref"] / lcs["amount_po"]
-            else:
-                lcs["exchange_rate"] = 0.0
-
         lc_summary = sorted(lc_summary_dict.values(), key=lambda x: x["date"] or "")
 
         return {
